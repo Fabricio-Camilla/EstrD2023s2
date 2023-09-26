7b2d65f9625c31b371456266972d007fbaf12681
Fabricio Camilla <142159089+Fabricio-Camilla@users.noreply.github.com>
2023-09-26 19:35:51 -0300

    Update

--- practida5/Stack/Stack.hs
+++ practida5/Stack/Stack.hs
@@ -2,34 +2,35 @@ module Stack
    (Stack, emptyS, isEmptyS, push, top, pop, lenS)
 where 
 
-data Stack a = Stack [a]
+data Stack a = Stack [a] Int 
 
     {-
         INV.REP:
             head, tail no pueden tener lista vacia.
+            n es la longitud de la lista
     -}
 
 
-emptyS :: Stack a
+emptyS :: Stack a Int
 --Crea una pila vacía.
-isEmptyS :: Stack a -> Bool
+isEmptyS :: Stack a Int -> Bool
 --Dada una pila indica si está vacía.
-push :: a -> Stack a -> Stack a
+push :: a -> Stack a Int -> Stack a Int
 --Dados un elemento y una pila, agrega el elemento a la pila.
-top :: Stack a -> a
+top :: Stack a Int -> a
 --Dada un pila devuelve el elemento del tope de la pila.
-pop :: Stack a -> Stack a
+pop :: Stack a Int -> Stack a Int
 --Dada una pila devuelve la pila sin el primer elemento.
-lenS :: Stack a -> Int
+lenS :: Stack a Int -> Int
 --Dada la cantidad de elementos en la pila.
 --Costo: constante.
 
 --Esto significa que los últimos elementos agregados a la estructura son los primeros en salir (como en una pila de
 --platos
 
-emptyS             = Stack []    --constante 
-isEmptyS (Stack a) = null a      --constante
-push x   (Stack a) = Stack (x:a) --constante
-top      (Stack a) = head a      --parcial -constante
-pop      (Stack a) = Stack (tail a) --parcial -constante
-lenS     (Stack a) = length a    --parcial - lineal
+emptyS               = Stack []    --constante 
+isEmptyS (Stack a n) = n== 0      --constante
+push x   (Stack a n) = Stack (x:a) (n+1)--constante
+top      (Stack a _) = head a      --parcial -constante
+pop      (Stack a n) = Stack (tail a) (n-1)--parcial -constante
+lenS     (Stack a n) = n    --constante
