cf3be845e33b710867a8e923b1678fda950a8974
Fabricio Camilla <142159089+Fabricio-Camilla@users.noreply.github.com>
2023-10-10 19:33:07 -0300

    Add files via upload

--- practica7/Empresa.hs
+++ practica7/Empresa.hs
@@ -9,25 +9,14 @@ data Empresa = ConsE (Map SectorId (Set Empleado))
 
 {-
     INV.REP.:
-        * cada empleado esta asociado a un cuil unico 
-        * un empleado puede pertenecer a mas de un sector y un sector puede tener mas de un empleado
-        * si hay un empleado asignado a un sector en el primer map 
-            dicho empleado tiene que estar en el segundo map asociado a un cuil.
+        * si  un empleado pertenece a un sector en el primer map 
+            dicho empleado tiene que estar asociado a un cuil en el segundo map.
+        * un empleado valor del segundo map, pertenece a los conjutnos asociados valor del primer map.
+        * para cada cuil c clave en el segundo map, asociado c a un empleado e. "cuil e" es igual a c.
 -}
 
 
-consEmpleado :: CUIL -> Empleado
---Propósito: construye un empleado con dicho CUIL.
---Costo: O(1)
-cuil :: Empleado -> CUIL
---Propósito: indica el CUIL de un empleado.
---Costo: O(1)
-incorporarSector :: SectorId -> Empleado -> Empleado
---Propósito: incorpora un sector al conjunto de sectores en los que trabaja un empleado.
---Costo: O(log S), siendo S la cantidad de sectores que el empleado tiene asignados.
-sectores :: Empleado -> SectorId
---Propósito: indica los sectores en los que el empleado trabaja.
---Costo: O(1)
+
 consEmpresa :: Empresa
 --Propósito: construye una empresa vacía.
 --Costo: O(1)
